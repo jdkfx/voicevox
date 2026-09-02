ccbdc1857d1e7213c17e4a8022b65e32564b955f
jdkfx <haruki.tazoe@gmail.com>
2026-09-03 00:00:06 +0900

    feat: エディタ選択結果を保存するContainerを追加

---
+++ src/components/Dialog/InitialSettingsDialog/Container.vue
@@ -0,0 +1,34 @@
+<template>
+  <Presentation v-model="isDialogOpen" @select="selectEditor" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import Presentation from "./Presentation.vue";
+import { useStore } from "@/store";
+import type { EditorType } from "@/type/preload";
+
+defineOptions({
+  name: "InitialSettingsDialog",
+});
+
+const store = useStore();
+
+const isDialogOpen = computed({
+  get: () => store.state.isInitialSettingsDialogOpen,
+  set: (value) =>
+    store.actions.SET_DIALOG_OPEN({
+      isInitialSettingsDialogOpen: value,
+    }),
+});
+
+const selectEditor = async (editor: EditorType) => {
+  await store.actions.SET_ROOT_MISC_SETTING({
+    key: "openedEditor",
+    value: editor,
+  });
+  await store.actions.SET_DIALOG_OPEN({
+    isInitialSettingsDialogOpen: false,
+  });
+};
+</script>
